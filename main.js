306ae217cfc019a40684766920c0170b1ef31c1f
MigiKTHZ <102576118+MigiKTHZ@users.noreply.github.com>
2023-10-16 12:30:41 +0200

    adjusted index to new content

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-var x = document.getElementById('BtnFile');
+const form = document.getElementById('uploadForm')
 
-x.addEventListener("click", myScript);
+const sendFiles = async () => {
+    // Object
+    const myFiles = document.getElementById('BtnFile').files
 
-function myScript() {
-    console.log('pressed');
-};
+    const formData = new FormData()
+
+    Object.keys(myFiles).forEach(key => {
+        formData.append(myFiles.item(key).name, myFiles.item(key))
+    })
+
+    const response = await fetch('http://dwaregateway.ddns.net:3000/upload', {
+        method: 'POST',
+        body: formData
+    })
+
+    const json = await response.json()
+
+    const h2 = document.getElementById('status')
+    h2.textContent = `Status: ${json?.status}`
+
+    const h3 = document.getElementById('msg')
+    h3.textContent = `Message: ${json?.message}`
+
+    console.log(json)
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    sendFiles()
+})
