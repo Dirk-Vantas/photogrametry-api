43244f60c2bfd263bd75bf1bfcdc3763058bfbba
MigiKTHZ <102576118+MigiKTHZ@users.noreply.github.com>
2023-10-23 01:09:33 +0200

    Updated some status

--- main.js
+++ main.js
@@ -41,6 +41,11 @@ const sendFiles = async () => {
         loadProgressbar(json?.hash);
     }, 5000);
 
+    document.getElementById('viewer').removeAttribute("src")
+    document.getElementById('viewer').removeAttribute("ios-src")
+    document.getElementById('viewer').setAttribute("src", "http://dwaregateway.ddns.net/jobs/44e61b45fb9e82dfc3213adebf1d05b1974ab4f87a867066a341c53bb69068dc354762ab91cfba9411910ab466cc8b756b8a2202fd4ac77d891c311b7eef87ac/model.glb")
+    document.getElementById('viewer').setAttribute("ios-src", "http://dwaregateway.ddns.net/jobs/44e61b45fb9e82dfc3213adebf1d05b1974ab4f87a867066a341c53bb69068dc354762ab91cfba9411910ab466cc8b756b8a2202fd4ac77d891c311b7eef87ac/model.glb")
+    enableDownloadButton()
 
     //    const h3 = document.getElementById('msg')
     //    h3.textContent = `Message: ${json?.message}`
@@ -63,11 +68,6 @@ const loadProgressbar = async (formbody) => {
     bar.setAttribute('value', json[3])
     
     if (json[3] == 100) {
-        document.getElementById('viewer').removeAttribute("src")
-        document.getElementById('viewer').removeAttribute("ios-src")
-        document.getElementById('viewer').setAttribute("src", "http://dwaregateway.ddns.net/jobs/44e61b45fb9e82dfc3213adebf1d05b1974ab4f87a867066a341c53bb69068dc354762ab91cfba9411910ab466cc8b756b8a2202fd4ac77d891c311b7eef87ac/model.glb")
-        document.getElementById('viewer').setAttribute("ios-src", "http://dwaregateway.ddns.net/jobs/44e61b45fb9e82dfc3213adebf1d05b1974ab4f87a867066a341c53bb69068dc354762ab91cfba9411910ab466cc8b756b8a2202fd4ac77d891c311b7eef87ac/model.glb")
-        enableDownloadButton()
         clearInterval()
     }
 }
