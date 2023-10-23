036a08013ca69367ef34f7598d444516b9cac9e4
Gideon Watson <gideon.watson@meteoblue.com>
2023-10-23 08:20:30 +0200

    try new loop

--- main.js
+++ main.js
@@ -37,15 +37,58 @@ const sendFiles = async () => {
 
     loading.innerHTML = ''
 
-    const intervalId = setInterval(function() {
-        loadProgressbar(json?.hash);
-    }, 5000);
+    // const intervalId = setInterval(function() {
+    //     loadProgressbar(json?.hash);
+    // }, 5000);
+
+    waitForProcess(json?.hash);
 
     //    const h3 = document.getElementById('msg')
     //    h3.textContent = `Message: ${json?.message}`
 
     console.log(json)
 }
+
+async function waitForProcess(hash) {
+    const done = 100;
+    return new Promise(resolve => {
+      const interval = setInterval(async () => {
+        try {
+          const response = await makeProgressAPIcall(json?.hash);
+          console.log('Current value:', response.value);
+          let progress = response.json[3];
+
+            bar.removeAttribute('value')
+            bar.setAttribute('value', progress)
+
+          if (progress === done) {
+            clearInterval(interval); // Stop the interval when the desired value is found
+            document.getElementById('viewer').removeAttribute("src")
+            document.getElementById('viewer').removeAttribute("ios-src")
+            document.getElementById('viewer').setAttribute("src", `http://dwaregateway.ddns.net/jobs/${hash}/model.glb`)
+            document.getElementById('viewer').setAttribute("ios-src", `http://dwaregateway.ddns.net/jobs/${hash}/model.glb`)
+            enableDownloadButton()
+
+            bar.removeAttribute('value')
+            bar.setAttribute('value', '0')
+
+            console.log('process Done');
+
+            resolve();
+          }
+        } catch (error) {
+          console.error('API call failed:', error);
+        }
+      }, 1000); // Repeat the API call every 1 second
+    });
+  }
+
+function makeProgressAPIcall(hash) {
+    // Make your API call here and return a promise
+    var url = 'http://dwaregateway.ddns.net:3000/status/' + formbody
+    return fetch(url)
+      .then(response => response.json());
+  }
 
 const loadProgressbar = async (formbody) => {
 
@@ -63,14 +106,7 @@ const loadProgressbar = async (formbody) => {
     
     if (json[3] == 100) {
         //set new shit
-        document.getElementById('viewer').removeAttribute("src")
-        document.getElementById('viewer').removeAttribute("ios-src")
-        document.getElementById('viewer').setAttribute("src", `http://dwaregateway.ddns.net/jobs/${formbody}/model.glb`)
-        document.getElementById('viewer').setAttribute("ios-src", `http://dwaregateway.ddns.net/jobs/${formbody}/model.glb`)
-        enableDownloadButton()
-
-        bar.removeAttribute('value')
-        bar.setAttribute('value', '0')
+        
         clearInterval(intervalId);
     }
 }
