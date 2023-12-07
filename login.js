3ccf81d0c99b668706baddc21f3ca468b798215f
MigiKTHZ <102576118+MigiKTHZ@users.noreply.github.com>
2023-12-07 08:44:24 +0100

    added register page

---
+++ login.js
@@ -0,0 +1,12 @@
+var usertxt = document.getElementById("usr");
+var pawdtxt = document.getElementById("pwd");
+const form = document.getElementById('loginForm')
+
+function submitLoginCredentials() {
+    
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    submitLoginCredentials()
+})
---
+++ register.js
@@ -0,0 +1,13 @@
+var usertxt = document.getElementById("usr");
+var pawdtxt = document.getElementById("pwd");
+var pawdtxt2 = document.getElementById("pwd2");
+const form = document.getElementById('registerForm')
+
+function submitLoginCredentials() {
+    
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    submitLoginCredentials()
+})
