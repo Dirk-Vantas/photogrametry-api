6308647e99443b445725b621eb63bce0b66955e7
Gideon Watson <gideon.watson@meteoblue.com>
2024-01-08 20:44:31 +0100

    Implemented a preliminary version of the user list feature. Next steps involve refining the interface to include a scrolling function, ensuring it doesn't occupy the entire screen. Similar updates are planned for the jobs display for a more user-friendly and space-efficient layout

--- public/userdashboard.js
+++ public/userdashboard.js
@@ -70,3 +70,46 @@ window.onload = function() {
     refreshJobData();  // Call once on page load
     setInterval(refreshJobData, 30000);  // Then call every 5 seconds
 };
+
+// Function to populate the table with user data
+function populateUserTable(data) {
+    const tableBody = document.getElementById('userTableBody');
+    if (tableBody != null){
+        tableBody.innerHTML = ''; // Clear existing table data
+        data.forEach(user => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${user.ID}</td>
+            <td>${user.Benutzername}</td>
+            <td>${user.Passwort}</td>
+            <td>${user.userlevel}</td>
+            <td><button class="btn btn-error"><i class="fa fa-trash-o" style="font-size:24px"></i></button></td>
+        `;
+        tableBody.appendChild(row);
+    });
+    }
+    
+}
+
+// Function to refresh user data
+function refreshUserData() {
+    fetch('/users')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            populateUserTable(data);
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
+
+// Call the function right after the page loads and then every 5 seconds
+window.onload = function() {
+    refreshUserData();  // Call once on page load
+    setInterval(refreshUserData, 100000);  // Then call every 5 seconds
+};
